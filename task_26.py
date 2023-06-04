df49d35d3ba87fa22c0db6702809bfbed652e605
Ylia <ukhrestina@yandex.ru>
2023-06-04 11:50:18 +0300

    Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии

---
+++ task_26.py
@@ -0,0 +1,20 @@
+# Задача 26:
+# Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+# *Пример:*
+
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    else:
+        return base * power(base, exponent - 1)
+
+A = int(input("Введите число A: "))
+B = int(input("Введите число B: "))
+
+result = power(A, B)
+print(result)
